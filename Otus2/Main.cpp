5c66e95a275f7d47131f6aaf9b8dce43fd9f5f4f
Gawluk <gavl.andr96@gmail.com>
2020-02-08 23:56:30 +0300

    return lambda

--- Otus2/Main.cpp
+++ Otus2/Main.cpp
@@ -48,32 +48,32 @@ int main(int argc, char const* argv[])
 
 		std::sort(ip_pool.begin(), ip_pool.end(), ip_pool_compare);
 
-		/*auto constTrue = [](const vect2String::const_iterator& iter, const uint8_t& firstNumb, const uint8_t& secondNumb) 
+		auto constTrue = [](const vect2String::const_iterator& iter, const uint8_t& firstNumb, const uint8_t& secondNumb) 
 							{
 								UNUSED(firstNumb);
 								UNUSED(secondNumb);
 								return true; 
 							};
-		show(ip_pool, constTrue, 0, 0);*/
+		show(ip_pool, constTrue, 0, 0);
 
 		uint8_t firstNumber = 1;
-		/*auto firstOketEql = [](const vect2String::const_iterator& iter, const uint8_t& firstNumb, const uint8_t& secondNumb) 
+		auto firstOketEql = [](const vect2String::const_iterator& iter, const uint8_t& firstNumb, const uint8_t& secondNumb) 
 								{
 									UNUSED(secondNumb);
 									return std::stoi(iter[0][0]) == firstNumb; 
 								};
-		show(ip_pool, firstOketEql, firstNumber, 0);*/
+		show(ip_pool, firstOketEql, firstNumber, 0);
 
 		firstNumber = 46;
 		uint8_t secondNumber = 70;
-		/*auto firstTwoOketEql = [](const vect2String::const_iterator& iter, const uint8_t& firstNumb, const uint8_t& secondNumb)
+		auto firstTwoOketEql = [](const vect2String::const_iterator& iter, const uint8_t& firstNumb, const uint8_t& secondNumb)
 								{
 									return  ((std::stoi(iter[0][0]) == firstNumb) && 
 											(std::stoi(iter[0][1]) == secondNumb));
 								};
-		show(ip_pool, firstTwoOketEql, firstNumber, secondNumber);*/
+		show(ip_pool, firstTwoOketEql, firstNumber, secondNumber);
 		
-		/*auto anyOketEql = [](const vect2String::const_iterator& iter,
+		auto anyOketEql = [](const vect2String::const_iterator& iter,
 								const uint8_t& firstNumb,
 								const uint8_t& secondNumb)
 							{
@@ -82,7 +82,7 @@ int main(int argc, char const* argv[])
 										(std::stoi(iter[0][1]) == firstNumb) ||
 										(std::stoi(iter[0][2]) == firstNumb) ||
 										(std::stoi(iter[0][3]) == firstNumb)); };
-		show(ip_pool, anyOketEql, firstNumber, 0);*/
+		show(ip_pool, anyOketEql, firstNumber, 0);
 
 	}
 	catch (const std::exception & e)
